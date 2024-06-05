721cbbadc02a924748a04e7f54e53f7caf778c35
Seb Scothorn <seb.scothorn@bjss.com>
2024-06-05 15:25:52 +0100

    initial commit

--- vessels/src/types/vessel.ts
+++ vessels/src/types/vessel.ts
@@ -1,3 +1,3 @@
 export type Vessel = {
-  name: string;
+	name: string;
 };
--- vessels/src/utils/file-reader/config.ts
+++ vessels/src/utils/file-reader/config.ts
@@ -1,13 +1,13 @@
 const config = {
-  books: {
-    type: "list",
-    worksheet: "Over_10_metres",
-    columns: [
-      {
-        index: 4, // column index (1,2,3....n); `1` for column `A`
-        key: "name", // output item's field
-      },
-    ],
-    rowOffset: 5, // offset header row
-  },
+	books: {
+		type: 'list',
+		worksheet: 'Over_10_metres',
+		columns: [
+			{
+				index: 4, // column index (1,2,3....n); `1` for column `A`
+				key: 'name', // output item's field
+			},
+		],
+		rowOffset: 5, // offset header row
+	},
 };
